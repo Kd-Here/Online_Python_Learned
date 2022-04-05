4aef181be0e10ec681c1e0b7442c2d079cba6b26
Kdhere <gamelover9899@gmail.com>
2022-04-05 11:39:37 +0530

    ListCompleted

---
+++ List.py
@@ -0,0 +1,29 @@
+#List can have different data-typees
+"""List is a collection which is ordered and changeable. Allows duplicate members.
+Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+Dictionary is a collection which is ordered** and changeable. No duplicate members."""
+from os import remove
+import this
+"""
+
+mylist=["list can have different data-type",9,9.899,True]
+print(mylist)
+
+#Case to print between the defined range
+print(mylist[2:4])
+
+#Case to print from starting to certain index
+print(mylist[:4])
+
+#Case to print certain index to end
+print(mylist[2:])
+
+#Case to check wherether the item present in list
+if "Kd" in mylist:
+    print(bool)
+else:
+    print("Not present")
+"""
+
+
