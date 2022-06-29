8a0f2a3ccf5625654a2ff218e380849daeddf68f
Kd-Here <gamelover9899@gmail.com>
2022-06-29 19:15:23 +0530

    Comparison of classes objects

--- Python-Classes & Objects/Python-use-of-self.py
+++ Python-Classes & Objects/Python-use-of-self.py
@@ -4,16 +4,39 @@
 
 class Car:
     # Defining attributes
-    def __init__(self,name, price, country):
-        self.name="Buggati"
+    def __init__(self):
+        self.name="Pagani" #  This are default parameter values we could change theml
         self.price="80M$"
         self.country="Itlay"
+        self.color="user-wish"
+    def comparison(self,other):
+        if self.name==other.name:
+            print("Both are same")
+        else:
+            print(f"{self.name} is the first car and another is {other.name} ")
     
     #Defining an methods
 
     def speed(self):
         self.speed="400Mph"
-        print("The speed of"+self.name+"is :~ " +self.speed)
+        print("The speed of "+self.name+"is :~ " +self.speed)
 
-Centodeicei=Car("centodièci","1B","Italy")
-Centodeicei.speed()
+c1=Car()
+c1.name="Buggati Centodièci"
+print(f'The name of Car is {c1.name}')
+c1.speed()
+
+Rolls_Royce=Car()
+Rolls_Royce.name="Rolls-Royce"
+print(f'The name of Car is {Rolls_Royce.name}')
+Rolls_Royce.speed()
+
+Zonda=Car()
+Zonda.color='white'
+print(f"The name of Car is {Zonda.name} which cost {Zonda.price } made in {Zonda.country} and it's color is {Zonda.color}")
+
+
+# We can't compare object in python by == sign bcoz it will simply compare the address of those object variable instead we want to compare the arguments of object create a funcion for that.
+
+
+Zonda.comparison(c1)
