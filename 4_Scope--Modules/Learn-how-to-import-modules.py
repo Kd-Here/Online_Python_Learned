f67db7939aa7efc34766d1ecdd52d023a78dfe1e
Kd-Here <gamelover9899@gmail.com>
2022-07-04 12:24:33 +0530

    Fibonaci series

--- 4_Scope--Modules/Learn-how-to-import-modules.py
+++ 4_Scope--Modules/Learn-how-to-import-modules.py
@@ -1,9 +1,11 @@
 # from Moudles import addon
+from ast import Pass
 from Moudles import *
 
 a=int(input("enter the first number:-"))
 b=int(input("enter the second number:-"))
 print(addon(a,b))
+
 
 # Here only addon funcgion is working bcox we imported only addon from moudles
 
@@ -14,3 +16,15 @@ print(addon(a,b))
 print(subon(a,b))
 print(mulon(a,b))
 print(divon(a,b))
+
+
+
+
+# -------------------Fibonaci code--------------------------------
+
+va=input("Type Yes if wanted to see Fibonaci series!")
+if va =="Yes":
+    Number=int(input("Enter the term till which we should print Fibonaci code:-\n"))
+    print(Fibo(Number))
+else:
+    print("Thnaks for uworking with us")
