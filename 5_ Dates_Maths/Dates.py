dcc03f9b87016b8301177e96ef28de3d2b9d3ec9
Kd-Here <gamelover9899@gmail.com>
2022-07-05 11:13:34 +0530

    Checking moduel content

--- 5_ Dates_Maths/Dates.py
+++ 5_ Dates_Maths/Dates.py
@@ -1,8 +1,4 @@
 import datetime
 
-x=datetime.datetime.now()
-print(x)
-print()
-print()
-print(x.year)
-print("hi")
+#datetime is module to check the content of any module use dir(module_name)
+print(dir(datetime))
